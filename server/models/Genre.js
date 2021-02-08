466cfa10cd50fcd68c5272a7bafb878d9bf9def5
jamieminh <48439646+jamieminh@users.noreply.github.com>
2021-02-08 12:55:25 +0700

    server get titles by multiple genres

--- server/models/Genre.js
+++ server/models/Genre.js
@@ -31,4 +31,12 @@ Media_Genre.belongsTo(Genre, {foreignKey: 'genre_id'});
 Media.hasMany(Media_Genre, {foreignKey: 'media_id'});
 Media_Genre.belongsTo(Media, {as: 'Media', foreignKey: 'media_id'});
 
-module.exports = {Genre, Media_Genre};
+const genre_ids = {action: 'act', adventure: 'adv', adult: 'adult', animation: 'ani', biography: 'bio',
+    comedy: 'com', crime: 'crm', documentary: 'doc', drama: 'dra', family: 'fam', fantasy: 'fns', 
+    film_noir: 'nir', game_show: 'gms', history: 'his', horror: 'hor', musical: 'mus',
+    mystery: 'mys', na: 'n/a', news: 'nws', reality_tv: 'rlt', romance: 'rom', sci_fi: 'sci-fi',
+    short: 'shr', 'sport': 'spr', talk_show: 'tlk', thriller: 'thr', war: 'war', western: 'wes'
+}
+
+
+module.exports = {Genre, Media_Genre, genre_ids};
--- server/routes/genres.js
+++ server/routes/genres.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { Genre, Media_Genres } = require('../models/Genre');
+const { Genre, Media_Genre, genre_ids } = require('../models/Genre');
+const { Review, Media_Reviewer } = require('../models/Reviewer')
 const Media = require('../models/Media');
+const { Sequelize, random } = require('../config');
 
 const router = express.Router();
+const Op = Sequelize.Op;
+
 
 
 // Get genres list
@@ -29,6 +33,84 @@ router.get('/:genre', (req, res) => {
         res.send(results)
     })
     .catch(err => console.log(err))
+})
+
+// get title with multiple genres
+router.get('/multiple/:genres', (req, res) => {
+    const genres = req.params.genres.split("+")
+    const hasRomance = false
+    const limit = genres.length * 300
+    const query_in = []
+    let same_genres = []
+
+    genres.map(genre => {
+        if (genres === 'romance')   
+            hasRomance = true
+        else 
+            query_in.push({genre_id: genre_ids[genre.replace('-', '_')]})
+    })
+
+    // put romance at the end bc there are alot of romance title
+    if (hasRomance)
+        query_in.push({genre_id: genre_ids['romance']})
+
+    // find 1000 result with one of the genre
+    Media_Genre.findAll({
+        where: { [Op.or]: query_in },
+        attributes: ['media_id'],
+        limit: limit
+    })
+    .then(results => {
+        let count = {}
+        // count the appearance of each id
+        for (let i=0; i<results.length; i++) {
+            const media_id = results[i].media_id
+            if (count[media_id] == null)        // if not visited, add it
+                count[media_id] = 1
+            else {                              // update value of a visited genre
+                const update = count[media_id] + 1
+                count[media_id] = update
+            }
+        }
+        
+        // add media_id of titles that have all genres
+        for (const key in count) {
+            if (count[key] == genres.length)
+                same_genres.push(key)
+        }
+
+        if (same_genres.length < 5) {
+            for (const key in count) {
+                if (count[key] == genres.length - 1)
+                    same_genres.push(key)
+                if (same_genres.length == 5)
+                    break
+            }
+        }
+
+    })
+    .then(r => {
+        // find information of ids found
+        console.log(same_genres.length);
+        res.send(same_genres)
+        
+        Media.findAll({
+            include: [{
+                model: Media_Reviewer,
+                attributes: ['score'],
+                where: { reviewer_id: 'im' }
+            }],
+            where: { 
+                media_id: {[Op.in] : same_genres}
+            },
+            attributes: ['media_id', 'title', 'released', 'poster_url'],
+        })
+        .then(results => {
+            res.send(results) 
+        })
+        .catch(err => console.error(err))
+    })
+    .catch(err => console.error(err))
 })
 
 module.exports = router
